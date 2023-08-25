5b476a52f80f55e54790b49cbf73d99ea118415b
Frank Gaede <frank.gaede@desy.de>
2023-08-25 14:54:31 +0200

    condition on angle in Par04ExampleVAE

--- src/Par04ExampleVAE.cc
+++ src/Par04ExampleVAE.cc
@@ -23,14 +23,15 @@ namespace ddml {
     G4ThreeVector position  = aFastTrack.GetPrimaryTrack()->GetPosition();
     G4ThreeVector direction = aFastTrack.GetPrimaryTrack()->GetMomentumDirection();
 
-    // here we could use position and direction to compute additional
-    // conditioning variables, such as incident angles ...
-    // for now assume simple GAN with 90 deg incident
     
+    // compute local incident angle
+    double theta = acos( localDir.z() ) ;
+
     if( DEBUGPRINT ) 
       std::cout << "  Par04ExampleVAE::prepareInput   pos0 = " << position
-		<< " - dir = " << direction << " - E = " << energy / CLHEP::GeV << std::endl ;
-    
+		<< " - dir = " << direction << " - E = " << energy / CLHEP::GeV
+		<< " theta = " << theta * 180. / M_PI
+		<< std::endl ;
     
     // the input for this model is the latent space and the energy conditioning
     
@@ -47,10 +48,10 @@ namespace ddml {
     /// Maximum particle energy value (in MeV) in the training range
     float fMaxEnergy = 1024000.0;
     /// Maximum particle angle (in degrees) in the training range
-    float fMaxAngle = 90.0;
+    float fMaxAngle = M_PI / 2. ; //90.0 deg ;
     
     inputs[0][_latentSize ]     = (energy /CLHEP::MeV) / fMaxEnergy ;
-    inputs[0][_latentSize + 1 ] = 1. ; //( direction.theta() /CLHEP::deg) / fMaxAngle ;
+    inputs[0][_latentSize + 1 ] = theta / fMaxAngle ;
     inputs[0][_latentSize + 2 ] = 0 ;
     inputs[0][_latentSize + 3 ] = 1 ;
 
@@ -90,9 +91,9 @@ namespace ddml {
 
       for(int j=0; j<_nCellsPhi; ++j){
 
-	float phi = (j+0.5) * 2.*CLHEP::pi / _nCellsPhi ;
-	float x = rho * cos( phi ) ;
-	float y = rho * sin( phi ) ;
+	float phiCell = (j+0.5) * 2.*CLHEP::pi / _nCellsPhi ;
+	float x = rho * cos( phiCell ) ;
+	float y = rho * sin( phiCell ) ;
 	
 	for(int l=0 ; l < nLayer ; ++l){
 
