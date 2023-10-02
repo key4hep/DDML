038a6698a6c695c9f3e7847a0d1cfec27c259c0d
Peter McKeown <mckeownp@naf-ilc11.desy.de>
2023-10-02 18:27:49 +0200

    Add setup for stable key4hep release

---
+++ setup_env_stable.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Script to setup key4hep stable envrionment that allows to use (py)torch, onnx and Marlin
+# together with DD4hep.
+#
+#
+
+source /cvmfs/ilc.desy.de/key4hep/releases/ca872f9c34/x86_64-ubuntu22.04-gcc11.3.0-opt/key4hep-stack/2023-09-28-hmnio4/setup.sh
+
+TORCH_PATH=$(dirname $(python -c 'import torch; print(f"{torch.__file__}")'))
+export CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:${TORCH_PATH}/share/cmake
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${TORCH_PATH}/lib
+
+ONNXRUNTIME_PATH=$(dirname $(python -c 'import onnxruntime; print(f"{onnxruntime.__file__}")'))
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(realpath ${ONNXRUNTIME_PATH}/../../../../)/lib64  # CentOS7
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$(realpath ${ONNXRUNTIME_PATH}/../../../../)/lib    # Ubuntu
+
+
