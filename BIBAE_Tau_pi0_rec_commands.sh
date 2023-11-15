1709b93467fec04771636095480ec2755088a5e8
peter-mckeown <github@petermckeown.com>
2023-11-15 12:08:16 +0100

    Add files for Tau simulation

---
+++ BIBAE_Tau_pi0_rec_commands.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+## commands to be run in singularity container
+
+source setup_env_stable.sh;
+
+### check build et al. are up to date and additional source
+#cd build;
+#cmake ..;
+#make -j$(nproc) install;
+#source install/bin/thisDDML.sh;
+#cd ./scripts/;
+
+cd ./ILDConfig/StandardConfig/production;
+
+#Marlin MarlinStdReco_WITH_GAPS.xml --constant.lcgeo_DIR=$lcgeo_DIR --constant.DetectorModel=ILD_l5_o1_v02 --constant.OutputBaseName=~/Tau_pi0_BIBAE_files/Rec/BIBAE_REC_tau_pi0_250-SetA_2f_leptonic_eL_pR --global.LCIOInputFiles=~/Tau_pi0_BIBAE_files/BIBAE_tau_pi0_250-SetA_2f_leptonic_eL_pR.slcio --constant.RunBeamCalReco=false;
+#Marlin MarlinStdReco_WITH_GAPS.xml --constant.DetectorModel=ILD_l5_o1_v02 --constant.OutputBaseName=~/Tau_pi0_BIBAE_files/Rec/BIBAE_REC_tau_pi0_250-SetA_2f_leptonic_eL_pR --global.LCIOInputFiles=~/Tau_pi0_BIBAE_files/BIBAE_tau_pi0_250-SetA_2f_leptonic_eL_pR.slcio --constant.RunBeamCalReco=false;
+
+#Marlin MarlinStdReco_WITH_GAPS.xml --constant.DetectorModel=ILD_l5_o1_v02 --constant.OutputBaseName=../../../Tau_pi0_BIBAE_files/Rec/BIBAE_REC_tau_pi0_250-SetA_2f_leptonic_eL_pR --global.LCIOInputFiles=../../../Tau_pi0_BIBAE_files/BIBAE_tau_pi0_250-SetA_2f_leptonic_eL_pR.slcio --constant.RunBeamCalReco=false;
+
+#### Reconstruction for two photon Tau samples
+Marlin MarlinStdReco_WITH_GAPS.xml --constant.DetectorModel=ILD_l5_o1_v02 --constant.OutputBaseName=../../../Tau_pi0_BIBAE_files/Rec/BIBAE_REC_tau_pi0_twophotons_trigger_250-SetA_2f_leptonic_eL_pR_file_61 --global.LCIOInputFiles=../../../Tau_pi0_BIBAE_files/BIBAE_tau_pi0_twophotons_trigger_250-SetA_2f_leptonic_eL_pR_file_61.slcio --constant.RunBeamCalReco=false;
---
+++ photon_geo_check_commands_G4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## commands to be run in singularity container
+
+source setup_env_stable.sh;
+
+### check build et al. are up to date and additional source
+#cd build;
+#cmake ..;
+#make -j$(nproc) install;
+source install/bin/thisDDML.sh;
+cd ./scripts/;
+
+# run the simulation
+ddsim --steeringFile ddsim_steer_photon_geo_check_G4.py --compactFile $lcgeo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml;
+
+#ddsim --steeringFile ddsim_steer.py --compactFile $lcgeo_DIR/ILD/compact/ILD_l5_o1_v02/ILD_l5_o1_v02.xml;
