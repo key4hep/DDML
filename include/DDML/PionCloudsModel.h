28e9767d8a1942b5b4fd0d1ba4f78d66ffaa60b3
peter-mckeown <74307317+peter-mckeown@users.noreply.github.com>
2025-11-20 13:34:24 +0100

    Update include/DDML/PionCloudsModel.h
    Co-authored-by: Thomas Madlener <thomas.madlener@desy.de>

--- include/DDML/PionCloudsModel.h
+++ include/DDML/PionCloudsModel.h
@@ -44,7 +44,6 @@ public:
 
 private:
   /// model properties for plugin
-  // These grid sizes were used for the two angle BIBAE
   int m_numPoints = 2600;                 // 4148; //2600; //number of points in the shower
   int m_latentSize = 3;                   // number of input features (energy, theta, phi)
   int m_maxNumElements = m_numPoints * 4; // number of space points in the output multiplied by 4 (x,y,z,energy)
