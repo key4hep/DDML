257d02557d83ac4be17a2a47880abe848340f509
Peter McKeown <peter.mckeown@cern.ch>
2026-04-22 15:27:33 +0200

    fix clang-tidy complaint

--- include/DDML/DDMLEventAction.h
+++ include/DDML/DDMLEventAction.h
@@ -28,35 +28,35 @@ public:
   void end(const G4Event*) override;
 
   //// Get and Set methods for Calo Face info
-  inline std::vector<G4int>& GetCaloMC_PDG() { return m_CaloMCPDG; }
-  inline std::vector<G4double>& GetCaloMC_E() { return m_CaloMCE; }
-  inline std::vector<G4double>& GetCaloMC_PosX() { return m_CaloMCPosX; }
-  inline std::vector<G4double>& GetCaloMC_PosY() { return m_CaloMCPosY; }
-  inline std::vector<G4double>& GetCaloMC_PosZ() { return m_CaloMCPosZ; }
-  inline std::vector<G4double>& GetCaloMC_DirX() { return m_CaloMCDirX; }
-  inline std::vector<G4double>& GetCaloMC_DirY() { return m_CaloMCDirY; }
-  inline std::vector<G4double>& GetCaloMC_DirZ() { return m_CaloMCDirZ; }
+  inline std::vector<G4int>& GetCaloMC_PDG() { return m_caloMCPDG; }
+  inline std::vector<G4double>& GetCaloMC_E() { return m_caloMCE; }
+  inline std::vector<G4double>& GetCaloMC_PosX() { return m_caloMCPosX; }
+  inline std::vector<G4double>& GetCaloMC_PosY() { return m_caloMCPosY; }
+  inline std::vector<G4double>& GetCaloMC_PosZ() { return m_caloMCPosZ; }
+  inline std::vector<G4double>& GetCaloMC_DirX() { return m_caloMCDirX; }
+  inline std::vector<G4double>& GetCaloMC_DirY() { return m_caloMCDirY; }
+  inline std::vector<G4double>& GetCaloMC_DirZ() { return m_caloMCDirZ; }
 
   // Set methods to push back vector
-  inline void SetElCaloMC_PDG(G4int aValue) { m_CaloMCPDG.push_back(aValue); }
-  inline void SetElCaloMC_E(G4double aValue) { m_CaloMCE.push_back(aValue); }
-  inline void SetElCaloMC_PosX(G4double aValue) { m_CaloMCPosX.push_back(aValue); }
-  inline void SetElCaloMC_PosY(G4double aValue) { m_CaloMCPosY.push_back(aValue); }
-  inline void SetElCaloMC_PosZ(G4double aValue) { m_CaloMCPosZ.push_back(aValue); }
-  inline void SetElCaloMC_DirX(G4double aValue) { m_CaloMCDirX.push_back(aValue); }
-  inline void SetElCaloMC_DirY(G4double aValue) { m_CaloMCDirY.push_back(aValue); }
-  inline void SetElCaloMC_DirZ(G4double aValue) { m_CaloMCDirZ.push_back(aValue); }
+  inline void SetElCaloMC_PDG(G4int aValue) { m_caloMCPDG.push_back(aValue); }
+  inline void SetElCaloMC_E(G4double aValue) { m_caloMCE.push_back(aValue); }
+  inline void SetElCaloMC_PosX(G4double aValue) { m_caloMCPosX.push_back(aValue); }
+  inline void SetElCaloMC_PosY(G4double aValue) { m_caloMCPosY.push_back(aValue); }
+  inline void SetElCaloMC_PosZ(G4double aValue) { m_caloMCPosZ.push_back(aValue); }
+  inline void SetElCaloMC_DirX(G4double aValue) { m_caloMCDirX.push_back(aValue); }
+  inline void SetElCaloMC_DirY(G4double aValue) { m_caloMCDirY.push_back(aValue); }
+  inline void SetElCaloMC_DirZ(G4double aValue) { m_caloMCDirZ.push_back(aValue); }
 
 private:
   // Fast Sim Calo entrace particle properties to store in ntuple
-  std::vector<G4int> m_CaloMCPDG;
-  std::vector<G4double> m_CaloMCE;
-  std::vector<G4double> m_CaloMCPosX;
-  std::vector<G4double> m_CaloMCPosY;
-  std::vector<G4double> m_CaloMCPosZ;
-  std::vector<G4double> m_CaloMCDirX;
-  std::vector<G4double> m_CaloMCDirY;
-  std::vector<G4double> m_CaloMCDirZ;
+  std::vector<G4int> m_caloMCPDG;
+  std::vector<G4double> m_caloMCE;
+  std::vector<G4double> m_caloMCPosX;
+  std::vector<G4double> m_caloMCPosY;
+  std::vector<G4double> m_caloMCPosZ;
+  std::vector<G4double> m_caloMCDirX;
+  std::vector<G4double> m_caloMCDirY;
+  std::vector<G4double> m_caloMCDirZ;
 };
 
 } // namespace ddml
